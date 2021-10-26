3740a205e74881cd67af87ab549752614c502010
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-10-26 15:21:07 -0400

    Create addsources.sh

---
+++ addsources.sh
@@ -0,0 +1,7 @@
+echo "Backing up sources.list";
+cp /etc/apt/sources.list /etc/apt/sources.list.bak -r;
+echo "Adding Kali Sources";
+echo deb http://kali.download/kali kali-rolling main contrib non-free >/etc/apt/sources.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ED444FF07D8D0BF6;
+echo "Updating...";
+apt update;
